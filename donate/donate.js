54efcb43540c976cf1f9afb00e13017fb5ffb2c3
Fedor Suhak <130792259+Kabanikov@users.noreply.github.com>
2024-03-23 02:37:33 +1100

    change related to the choice of donation quantity

--- donate/donate.js
+++ donate/donate.js
@@ -33,3 +33,29 @@ progress_bar_prices.addEventListener('click', e=>{
         progress_bar_points.children[selected_index].classList.toggle('pick_and_feed__progress_bar-point-selected');
     }
 })
+
+const donate_field = document.getElementById('pick_and_feed__custom-donation-field');
+const donate_button = document.querySelector('.pick_and_feed__button-feed');
+const donate_regex = /^[1-9][0-9]*$/g
+donate_field.value="";
+
+donate_button.disabled=true;
+donate_button.style.cursor='not-allowed';
+donate_button.style.background='gray';
+donate_field.style.borderColor='rgb(254, 143, 24)';
+
+donate_field.addEventListener("input", e=>{
+    let input_text = e.target.value;
+    if(input_text.length===0){
+        donate_button.disabled=true;
+        donate_button.style.cursor='not-allowed';
+        donate_field.style.borderColor='rgb(254, 143, 24)';
+        return;
+    }
+    let isValid = input_text.match(donate_regex);
+    donate_button.disabled=!isValid;
+    donate_field.style.borderColor = (isValid?'green':'red');
+    donate_button.style.cursor=(isValid?'pointer':'not-allowed');
+    donate_button.style.background='';
+
+})
